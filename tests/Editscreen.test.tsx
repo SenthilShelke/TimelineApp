8f19e7310982df4dfa6c0d8600551de6dce226a8
SenthilShelke <senthil.shelke@gmail.com>
2025-02-15 16:02:28 -0500

    added test for automatically bringing up text input when going into edit screen

--- tests/Editscreen.test.tsx
+++ tests/Editscreen.test.tsx
@@ -1,21 +1,38 @@
 import React from "react";
-import { fireEvent, render } from "@testing-library/react-native";
+import { act, render, fireEvent } from "@testing-library/react-native";
 import EditScreen from "../app/EditScreen";
 import { navigate } from "expo-router/build/global-state/routing";
 
-describe("<EditScreen />", () => {
-    it("navigates to the previous screen when the back button is pressed", () => {
-        const mockNavigation = {
-          navigate: jest.fn(),
-          goBack: jest.fn(),
-        };
-    
-        const { getByText } = render(<EditScreen navigation={mockNavigation} />);
-        const button = getByText("<"); 
-    
-        fireEvent.press(button);
-    
-        expect(mockNavigation.goBack).toHaveBeenCalled(); 
-      });
+jest.useFakeTimers();
 
+describe("<EditScreen />", () => {
+  it("navigates to the previous screen when the back button is pressed", () => {
+    const mockNavigation = {
+      navigate: jest.fn(),
+      goBack: jest.fn(),
+    };
+
+    const { getByText } = render(<EditScreen navigation={mockNavigation} />);
+    const button = getByText("<");
+
+    fireEvent.press(button);
+
+    expect(mockNavigation.goBack).toHaveBeenCalled();
+  });
+
+  it("automatically focuses the title TextInput when the screen loads", async () => {
+    const mockNavigation = {
+      navigate: jest.fn(),
+      goBack: jest.fn(),
+    };
+
+    const { getByPlaceholderText } = render(<EditScreen navigation={mockNavigation} />);
+    const titleInput = getByPlaceholderText("Title");
+
+    await act(async () => {
+      jest.runAllTimers(); 
+    });
+
+    expect(titleInput).toBeTruthy(); 
+  });
 });
